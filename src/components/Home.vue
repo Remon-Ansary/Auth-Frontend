f483559af31f859f2d7bab9e8b197ecd7bdcbecd
Md. Rasel Ansary Remon <remonansary@gmail.com>
2022-08-03 18:32:08 +0600

    home page navbar added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
-    <h1>{{ useremail }}</h1>
+    <!-- <h1>{{ useremail }}</h1>
     <h1>{{ userRole }}</h1>
-    <input type="button" value="Logout" @click="logout" />
+    <input type="button" value="Logout" @click="logout" /> -->
+    <nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top">
+      <div class="container">
+        <a href="/" class="navbar-brand">
+          {{ useremail }}
+        </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="" class="nav-link active"> Home </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="" class="nav-link active"> {{ userRole }} </a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link active">
+                <input type="button" value="Logout" @click="logout" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 <script>
